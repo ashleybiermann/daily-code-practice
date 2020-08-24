78ee2b5dc21cdd909ca54f0b625c539ca952d27b
ASHLEY BIERMANN <ashleybiermann@gmail.com>
2020-08-24 14:03:24 -0700

    completed challenge and testing for camel case

--- camel-case/challenge.js
+++ camel-case/challenge.js
@@ -1,22 +1,25 @@
-function toCamelCase(str){
+function toCamelCase(str) {
   let result = '';
-  
+  let removeMe = '';
+
   if (!str) {
     return result;
   }
-  
+
   result += str[0];
-  
+
   for (let i = 1; i < str.length; i++) {
-    
     if (str[i] === '-' || str[i] === '_') {
-      str.replace(i,'');
       let change = str[i + 1].toUpperCase();
       result += change;
-    } 
-    //FIXME: need to account for remaining letters
+      removeMe = str[i + 1];
+    } else {
+      let remaining = str[i];
+      if (remaining !== removeMe) {
+      result += remaining;
+      }
+    }
   }
-  
   return result;
 }
 
