8b5d198ae0b08ae182cc7227b41c20a3fbb702a3
ASHLEY BIERMANN <ashleybiermann@gmail.com>
2020-08-26 16:39:44 -0700

    adds challenge, but not passing tests yet. Incomplete

--- equal-sides-of-array/challenge.js
+++ equal-sides-of-array/challenge.js
@@ -1,7 +1,44 @@
 'use strict';
 
+// FIXME: this currently adds the lefts first then the rights, and doens't yet give the expected output
+function findEvenIndex(arr) {
+  let leftSum = 0;
+  let rightSum = 0;
+  let result = -1;
+  let pointer = 0;
 
 
+  for(let i = 0; i < arr.length; i++) {
+
+    // add left side
+    if(pointer < arr[i]) {
+      leftSum += arr[i];
+      console.log('leftSum: ', leftSum);
+    }
+
+    // add right side
+    if(pointer > arr[i]) {
+      rightSum += arr[i];
+      console.log('rightSum: ', rightSum);
+    }
+
+    // after left and right have been totaled, check for match
+    if (leftSum === rightSum) {
+      result = i;
+      return result;
+    } else {
+    // move the pointer one more index forward
+    pointer++;
+    console.log('pointer going forward to index ', pointer);
+    }
+  }
+
+  return result;
+}
+
+
+
+findEvenIndex([1,2,3,4,3,2,1]);
 
 
 module.exports = findEvenIndex;
